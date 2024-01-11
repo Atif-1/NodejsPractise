ec2522a9f2e42fd4f9de0a7abb27aa760729b818
Atif-1 <atif1712001@gmail.com>
2024-01-11 15:53:27 +0530

    Reading and file showing it above the form

--- serverDemo.js
+++ serverDemo.js
@@ -3,15 +3,24 @@ const fs=require('fs');
 const server=http.createServer((req,res)=>{
 	const url=req.url;
 	const method=req.method;
+	const message=fs.readFileSync('message.txt',(err)=>console.log("cannot read file data"+err));;
 	if(url==='/'){
 	res.write('<html>');
 	res.write('<head><title>My First Server</title></head>');
-	res.write('<body><form action="/message" method ="POST"><input type="text" name="message"><input type="submit"></form></body>')
+	res.write(`<body><p>${message}</p><form action="/message" method ="POST"><input type="text" name="message"><input type="submit"></form></body>`)
 	res.write('</html>');
 	return res.end();
 	}
 	if(url=='/message' && method=='POST'){
-		fs.writeFileSync('message.txt','DUMMY');
+		const body=[];
+		req.on('data',(chunks)=>{
+			body.push(chunks);
+		})
+		req.on('end',()=>{
+			const parsedBody=Buffer.concat(body).toString();
+			const msg=parsedBody.split('=')[1];
+			fs.writeFileSync('message.txt',msg);
+		})
 		res.statusCode=302;
 		res.setHeader('Location','/');//to  set back the page url to slash
 		return res.end();
